c494fa9e96df179134a0c591e47bda249c321af5
Marcos Aurélio Macedo da Rocha Júnior <marcosau_2809@hotmail.com>
2018-06-05 23:01:13 -0300

    Commit inicial do Projeto

---
+++ src/main/java/br/ufrn/ws/eitacasei/EitaCaseiService/EitaCaseiServiceApplication.java
@@ -0,0 +1,12 @@
+package br.ufrn.ws.eitacasei.EitaCaseiService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitaCaseiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EitaCaseiServiceApplication.class, args);
+	}
+}
